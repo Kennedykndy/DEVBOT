588ec8188dd50c6d07770fa7a396d1bddd300896
Kennedykndy <dev.senac.kennedy@gmail.com>
2026-05-06 11:56:10 -0300

    Atualizando Projeto

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const { error } = require("console");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
